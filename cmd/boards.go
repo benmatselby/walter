06f1cf9569e37043fabf54abb89cf36d98951de0
Ben Selby <benmatselby@gmail.com>
2018-11-02 21:34:48 +0000

    Add some new commands

--- cmd/boards.go
+++ cmd/boards.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"sort"
 
-	"github.com/spf13/viper"
-
 	jira "github.com/andygrunwald/go-jira"
 	"github.com/spf13/cobra"
 )
@@ -16,17 +14,7 @@ var boardsCmd = &cobra.Command{
 	Use:   "boards",
 	Short: "List all the boards in Jira",
 	Run: func(cmd *cobra.Command, args []string) {
-		base := viper.GetString("JIRA_URL")
-		tp := jira.BasicAuthTransport{
-			Username: viper.GetString("JIRA_USERNAME"),
-			Password: viper.GetString("JIRA_TOKEN"),
-		}
-
-		client, err := jira.NewClient(tp.Client(), base)
-		if err != nil {
-			fmt.Fprint(os.Stderr, err)
-			os.Exit(2)
-		}
+		client := getClient()
 
 		opts := jira.BoardListOptions{}
 		list, _, err := client.Board.GetAllBoards(&opts)
@@ -46,14 +34,21 @@ var boardsCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(boardsCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+func getBoard(name string) (*jira.Board, error) {
+	client := getClient()
+	opts := jira.BoardListOptions{}
+	list, _, err := client.Board.GetAllBoards(&opts)
+	if err != nil {
+		return nil, err
+	}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// boardsCmd.PersistentFlags().String("foo", "", "A help for foo")
+	for _, item := range list.Values {
+		if item.Name == name {
+			return &item, nil
+		}
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// boardsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	return nil, fmt.Errorf("unable to find board with name %s", name)
 }
---
+++ cmd/sprint.go
@@ -0,0 +1,97 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	jira "github.com/andygrunwald/go-jira"
+	"github.com/spf13/cobra"
+)
+
+var sprintIssuesCmd = &cobra.Command{
+	Use:   "issues",
+	Short: "List all the issues for the sprint",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		boardName := args[0]
+		sprintName := args[1]
+
+		issues, err := getIssues(boardName, sprintName)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(2)
+		}
+
+		for _, issue := range issues {
+			fmt.Println(fmt.Sprintf("* %s", issue.Fields.Summary))
+		}
+	},
+}
+
+var sprintListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all the sprints",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		boardName := args[0]
+
+		board, err := getBoard(boardName)
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(2)
+		}
+
+		client := getClient()
+		sprints, _, err := client.Board.GetAllSprints(strconv.Itoa(board.ID))
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+			os.Exit(2)
+		}
+
+		for _, sprint := range sprints {
+			fmt.Println(fmt.Sprintf("* %s", sprint.Name))
+		}
+	},
+}
+
+// sprintCmd represents the sprint command
+var sprintCmd = &cobra.Command{
+	Use:   "sprint",
+	Short: "Sprint related commands",
+}
+
+func init() {
+	sprintCmd.AddCommand(sprintIssuesCmd)
+	sprintCmd.AddCommand(sprintListCmd)
+	rootCmd.AddCommand(sprintCmd)
+}
+
+func getIssues(boardName, sprintName string) ([]jira.Issue, error) {
+	client := getClient()
+
+	board, err := getBoard(boardName)
+	if err != nil {
+		return nil, err
+	}
+
+	sprints, _, err := client.Board.GetAllSprints(strconv.Itoa(board.ID))
+	if err != nil {
+		return nil, err
+	}
+
+	sprintID := -1
+	for _, sprint := range sprints {
+		if sprint.Name == sprintName {
+			sprintID = sprint.ID
+			break
+		}
+	}
+
+	issues, _, err := client.Sprint.GetIssuesForSprint(sprintID)
+	if err != nil {
+		return nil, err
+	}
+
+	return issues, nil
+}
