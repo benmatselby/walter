57b14a49459ed2714b80683d9243911f38a3aef8
Ben Selby <benmatselby@gmail.com>
2018-12-08 11:02:17 +0000

    Only show the config file output if there is an error

--- cmd/root.go
+++ cmd/root.go
@@ -73,7 +73,7 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load config: %s\n", err)
 	}
 }
